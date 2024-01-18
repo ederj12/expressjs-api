e8bbf2dba0aea1f2601c73755c9a4d9d446f636e
Eder Vásquez <be.oono.app@gmail.com>
2024-01-17 18:36:10 -0600

    Add error handler and migrate routes handlers to routers

--- src/app.js
+++ src/app.js
@@ -1,21 +1,22 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
+const { sequelize } = require('./model');
+const { contractsRouter, jobsRouter, profilesRouter } = require('./routes');
+const morgan = require('morgan');
 const app = express();
-app.use(bodyParser.json());
-app.set('sequelize', sequelize)
-app.set('models', sequelize.models)
+app.use(express.json());
+app.set('sequelize', sequelize);
+app.set('models', sequelize.models);
+app.use(morgan('tiny'));
 
-/**
- * FIX ME!
- * @returns contract by id
- */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
-    res.json(contract)
-})
+app.use('/profiles', profilesRouter);
+app.use('/contracts', contractsRouter);
+app.use('/jobs', jobsRouter);
+//error handler
+app.use((err, req, res, next) => {
+  if (err.stack) {
+    return res.status(500).send('Something broke!');
+  }
+  res.status(err.status).json(err);
+});
+
 module.exports = app;
