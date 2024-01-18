02dbcb1616bb64451158d48c6b3f82dd6a3f046d
Eder Vásquez <be.oono.app@gmail.com>
2024-01-18 16:09:33 -0600

    Add new routes

--- src/app.js
+++ src/app.js
@@ -1,4 +1,10 @@
-const { contractsRouter, jobsRouter, profilesRouter } = require('./routes');
+const {
+  adminRouter,
+  balancesRouter,
+  contractsRouter,
+  jobsRouter,
+  profilesRouter
+} = require('./routes');
 const { db } = require('../DB');
 const dotenv = require('dotenv');
 const express = require('express');
@@ -11,15 +17,16 @@ dotenv.config();
 app.use(express.json());
 app.set('sequelize', db.sequelize);
 app.set('models', db.sequelize.models);
-app.use(morgan('tiny'));
-
-app.use('/profiles', profilesRouter);
+app.use(morgan('dev'));
+app.use('/admin', adminRouter);
+app.use('/balances', balancesRouter);
 app.use('/contracts', contractsRouter);
 app.use('/jobs', jobsRouter);
+app.use('/profiles', profilesRouter);
 //error handler
 app.use((err, _req, res, _next) => {
   if (err.stack) {
-    return res.status(500).send('Something broke!');
+    return res.status(500).send(err.stack);
   }
   res.status(err.status).json(err);
 });
