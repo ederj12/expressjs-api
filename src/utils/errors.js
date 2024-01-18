b3b5ade3c56b8ef0f6d734ac07ea63f053181d5f
Eder Vásquez <be.oono.app@gmail.com>
2024-01-17 18:28:35 -0600

    Add Utils

---
+++ src/utils/errors.js
@@ -0,0 +1,8 @@
+module.exports = {
+  notFound: { status: 404, message: 'Not Found Contracts' },
+  balanceError: {
+    status: 400,
+    message: 'There is not enough founds in your balance'
+  },
+  jobPaid: { status: 400, message: 'Job Paid' }
+};
