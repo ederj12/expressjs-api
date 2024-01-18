f632e8a863cd5e52f3d0f8657dc8ca1dfd3be313
Eder Vásquez <be.oono.app@gmail.com>
2024-01-18 15:57:14 -0600

    Add errors

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -4,5 +4,10 @@ module.exports = {
     status: 400,
     message: 'There is not enough founds in your balance'
   },
-  jobPaid: { status: 400, message: 'Job Paid' }
+  jobPaid: { status: 400, message: 'Job Paid' },
+  amountError: {
+    status: 400,
+    message: 'Deposit could not be more than 25% his total of jobs to pay'
+  },
+  badRequest: { status: 400, message: 'Bad Request' }
 };
