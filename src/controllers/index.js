8ae8513a3f90e28c25de49ea487d7a819fb3f740
Eder Vásquez <be.oono.app@gmail.com>
2024-01-18 16:09:17 -0600

    Add admin controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,9 @@
+const adminController = require('./admin');
 const balancesController = require('./balances');
 const contractsController = require('./contracts');
 const jobsController = require('./jobs');
 module.exports = {
+  adminController,
   balancesController,
   contractsController,
   jobsController
