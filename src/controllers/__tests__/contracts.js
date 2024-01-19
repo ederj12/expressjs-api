28b6f2ae74e16e1c2723a9a724d08735051e567f
Eder Vásquez <be.oono.app@gmail.com>
2024-01-18 21:22:26 -0600

    Add controller test

---
+++ src/controllers/__tests__/contracts.js
@@ -0,0 +1,28 @@
+const { contractsController } = require('../index');
+const { db } = require('../../../DB');
+const { errors } = require('../../utils');
+const { Contract } = db.sequelize.models;
+
+describe('', () => {
+  beforeEach(async () => {
+    //seed
+  });
+  test('Should return contract with id 1', async () => {
+    const id = 1;
+    const profile = { id: 1 };
+    const data = await contractsController.getContractById(
+      id,
+      profile,
+      Contract
+    );
+    expect(data.id).toBe(id);
+  });
+  test('Should return not found contracts error', async () => {
+    const id = 100;
+    const profile = { id: 90 };
+
+    await expect(
+      contractsController.getContractById(id, profile, Contract)
+    ).rejects.toEqual(errors.notFound);
+  });
+});
